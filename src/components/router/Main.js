50c1c46b5c85e9b19987e9b0622c45ae23443c69
Ben <belson1988@gmail.com>
2021-08-13 04:27:58 +0300

    ben

--- src/components/router/Main.js
+++ src/components/router/Main.js
@@ -1,63 +1,61 @@
-import {Redirect, Route, Switch, useHistory} from "react-router-dom";
+import { Redirect, Route, Switch, useHistory } from "react-router-dom";
 import LoginSignUp from "../sliced/auth/LoginSignUp";
 import Navbar from "../sliced/auth/Navbar";
 import RouteGuard from "./RouteGuard";
 import Home from "../router/Home";
-import React, {useEffect} from "react";
-import {Auth, Hub} from "aws-amplify";
-import {useDispatch, useSelector} from "react-redux";
-import {setActiveUser} from "../../stores/slices/authSlicer";
-
+import React, { useEffect } from "react";
+import { Auth, Hub } from "aws-amplify";
+import { useDispatch, useSelector } from "react-redux";
+import { setActiveUser } from "../../stores/slices/authSlicer";
 
 export default () => {
-    const {username} = useSelector(({auth}) => auth);
-    const history = useHistory();
-    const dispatch = useDispatch()
-    useEffect(() => {
-        Auth.currentSession()
-            .then(({idToken: {payload}}) => {
-                debugger
-                dispatch(setActiveUser(payload))
-                // console.log(payload);
-                // debugger;
-            })
-    }, [])
+  const { username } = useSelector(({ auth }) => auth);
+  const history = useHistory();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    Auth.currentSession().then(({ idToken: { payload } }) => {
+      //debugger
+      dispatch(setActiveUser(payload));
+      // console.log(payload);
+      // debugger;
+    });
+  }, []);
 
-    useEffect(() => {
-        //debugger
-        if (username) {
-            history.push("/home");
-        }
-    }, [username]);
+  useEffect(() => {
+    //debugger
+    if (username) {
+      history.push("/home");
+    }
+  }, [username]);
 
-    // useEffect(() => {
-    //     Hub.listen("auth", (data) => {
-    //         const {payload} = data;
-    //         console.log("A new auth event has happened: ", data);
-    //         if (payload.event === "signIn") {
-    //             console.log("a user has signed in!");
-    //             console.log(payload.data);
-    //         }
-    //         if (payload.event === "signOut") {
-    //             console.log("a user has signed out!");
-    //         }
-    //     });
-    // }, []);
+  // useEffect(() => {
+  //     Hub.listen("auth", (data) => {
+  //         const {payload} = data;
+  //         console.log("A new auth event has happened: ", data);
+  //         if (payload.event === "signIn") {
+  //             console.log("a user has signed in!");
+  //             console.log(payload.data);
+  //         }
+  //         if (payload.event === "signOut") {
+  //             console.log("a user has signed out!");
+  //         }
+  //     });
+  // }, []);
 
-    return (
-        <>
-            <Navbar/>
-            <Switch>
-                <Route exact path="/">
-                    <Redirect to="/home"/>
-                </Route>
-                <Route exact path="/auth">
-                    <LoginSignUp/>
-                </Route>
-                <RouteGuard path="/home">
-                    <Home/>
-                </RouteGuard>
-            </Switch>
-        </>
-    );
+  return (
+    <>
+      {username && <Navbar />}
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+        <Route exact path="/auth">
+          <LoginSignUp />
+        </Route>
+        <RouteGuard path="/home">
+          <Home />
+        </RouteGuard>
+      </Switch>
+    </>
+  );
 };
