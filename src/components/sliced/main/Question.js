3a31abadfbe0b8e04a8f8d403e5149e832d187dd
Ben <belson1988@gmail.com>
2021-08-22 23:30:19 +0300

    Ben

--- src/components/sliced/main/Question.js
+++ src/components/sliced/main/Question.js
@@ -1,6 +1,17 @@
+import { useEffect, useState } from "react";
+import ExpandButton from "../../styled/ExpandButton";
+import MenuButton from "../../styled/MenuButton";
+
 export default (props) => {
   console.log(props);
+  const [isOpen, setSinOpen] = useState(false);
+  const [expand, setExpand] = useState("More details");
+  useState(() => {}, [isOpen]);
 
+  const toggleOpendiv = () => {
+    setSinOpen(!isOpen);
+    setExpand(expand === "More details" ? "Close" : "More details");
+  };
   return (
     <div
       style={{
@@ -9,8 +20,28 @@ export default (props) => {
         padding: "10px",
         marginTop: "10px",
         borderRadius: "15px",
+        position: "relative",
       }}
     >
+      <ExpandButton
+        style={{
+          position: "absolute",
+          top: "10px",
+          right: "30px",
+          fontSize: "15px",
+          fontWeight: "bold",
+          borderRadius: "10px",
+          margin: "20px",
+          padding: "10px",
+          color: "white",
+          backgroundImage:
+            " linear-gradient(135deg, rgba(0,45,50,1) 0%, rgba(0,0,0,0.865983893557423) 95%)",
+          border: "none",
+          boxShadow: "0px 0px 0px 1px #7e7c7c",
+        }}
+      >
+        Edit Question
+      </ExpandButton>
       <img
         src={props.imageUrl}
         alt={"png"}
@@ -19,80 +50,99 @@ export default (props) => {
       <h2 style={{ color: "white" }}>
         {props.index + 1}. {props.body}
       </h2>
-
-      <div>
-        <h3 style={{ color: "lightgray", marginBottom: "-5px" }}>Answers</h3>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            textAlign: "center",
-            justifyContent: "center",
-          }}
-        >
-          {props.answers.map((answer) => {
-            return (
-              <div>
-                <p
-                  style={{
-                    minWidth: "40px",
-                    fontWeight: "bolder",
-                    paddingRight: "5px",
-                    paddingLeft: "5px",
-                    paddingTop: "10px",
-                    paddingBottom: "10px",
-                    marginLeft: "20px",
-                    marginRight: "20px",
-                    color: "white",
-                    borderRadius: "15px",
-                    boxShadow: "0 0 0 1pt grey",
-                    backgroundImage:
-                      "linear-gradient(135deg, rgba(64,85,83,0.6334908963585435) 0%, rgba(0,51,54,0.6474964985994398) 85%)",
-                  }}
-                >
-                  {answer.body}
-                </p>
-                {answer.iceBreaker === "" ? (
-                  ""
-                ) : (
-                  <h5 style={{ color: "lightgray", marginBottom: "-5px" }}>
-                    Ice Breaker-
+      <ExpandButton
+        style={{
+          fontSize: "15px",
+          fontWeight: "bold",
+          borderRadius: "10px",
+          margin: "20px",
+          padding: "10px",
+          color: "white",
+          backgroundImage:
+            " linear-gradient(135deg, rgba(0,45,50,1) 0%, rgba(0,0,0,0.865983893557423) 95%)",
+          border: "none",
+          boxShadow: "0px 0px 0px 1px #7e7c7c",
+        }}
+        onClick={toggleOpendiv}
+      >
+        {expand}
+      </ExpandButton>
+      {isOpen && (
+        <div>
+          <h3 style={{ color: "lightgray", marginBottom: "-5px" }}>Answers</h3>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              textAlign: "center",
+              justifyContent: "center",
+            }}
+          >
+            {props.answers.map((answer) => {
+              return (
+                <div>
+                  <p
+                    style={{
+                      minWidth: "40px",
+                      fontWeight: "bolder",
+                      paddingRight: "5px",
+                      paddingLeft: "5px",
+                      paddingTop: "10px",
+                      paddingBottom: "10px",
+                      marginLeft: "20px",
+                      marginRight: "20px",
+                      color: "white",
+                      borderRadius: "15px",
+                      boxShadow: "0 0 0 1pt grey",
+                      backgroundImage:
+                        "linear-gradient(135deg, rgba(64,85,83,0.6334908963585435) 0%, rgba(0,51,54,0.6474964985994398) 85%)",
+                    }}
+                  >
+                    {answer.body}
+                  </p>
+                  {answer.iceBreaker === "" ? (
+                    ""
+                  ) : (
+                    <h5 style={{ color: "white", marginBottom: "-5px" }}>
+                      Ice Breaker-
+                    </h5>
+                  )}
+                  <p
+                    style={{
+                      color: "lightgray",
+                      fontSize: "13px",
+                      paddingRight: "10px",
+                      paddingLeft: "10px",
+                    }}
+                  >
+                    {answer.iceBreaker}
+                  </p>
+                  <h5
+                    style={{
+                      margin: "0",
+                      padding: "0",
+                    }}
+                  >
+                    Effects-
                   </h5>
-                )}
-                <p
-                  style={{
-                    color: "white",
-                    fontSize: "13px",
-                    paddingRight: "10px",
-                    paddingLeft: "10px",
-                  }}
-                >
-                  {answer.iceBreaker}
-                </p>
-                <h6
-                  style={{
-                    margin: "0",
-                    padding: "0",
-                  }}
-                >
-                  Effects-
-                </h6>
-                {answer.effects.map((effect) => {
-                  return (
-                    <p
-                      style={{
-                        fontSize: "10px",
-                      }}
-                    >
-                      {effect.feature} - {effect.value}
-                    </p>
-                  );
-                })}
-              </div>
-            );
-          })}
+                  {answer.effects.map((effect) => {
+                    return (
+                      <p
+                        style={{
+                          fontSize: "13px",
+                          color: "lightgray",
+                        }}
+                      >
+                        {effect.feature} - {effect.value}
+                      </p>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
